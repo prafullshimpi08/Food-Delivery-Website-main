adec8a9fc76d6ef04c897c3ccd9488504a831fac
prafull <prafullshimpi2829@.com>
2024-08-01 18:12:52 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+
+
+
+# Getting Started
+
+- First of all, you will need to create a database with Atlas MongoDB.
+- Also have a Stripe account so you can get your SECRET STRIPE KEY.
+
+
+# Installation
+
+<h2>How to install/setup the Front-End</h2>
+
+> Open a terminal in VS Code and go into the frontend folder and then run
+
+>❗Open the frontend first [http://localhost:5173](http://localhost:5173)
+
+
+```
+npm install
+npm run dev
+```
+
+
+<h2>How to install/setup the Admin Page</h2>
+
+> Open a terminal in VS Code and go into the admin folder and then run
+
+>❗Open the admin after the frontend. [http://localhost:5174](http://localhost:5174)
+
+```
+npm install
+npm run dev
+```
+   
+<h2>How to install/setup the Backend</h2>
+
+> Open a terminal in VS Code and go into the backend folder and then run
+
+```
+npm start server
+```
+
+- [ ] Remember to include your Database in db.js and your Stripe Secret Key in .env
---
+++ backend/server.js
@@ -0,0 +1,50 @@
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+// app config
+const app = express()
+const port =  4000;
+
+
+
+// middlewares
+app.use(express.json())
+
+const allowedOrigins = [
+    "https://food-delivery-website-gamma.vercel.app",
+    "https://food-delivery-website-admin.vercel.app"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+}));
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
